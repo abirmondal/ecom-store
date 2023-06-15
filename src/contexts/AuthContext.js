0aa29feb9da90108483f07b2e8743f76983f67dd
Abir Mondal <abir22197072@gmail.com>
2023-06-15 16:57:35 +0530

    Change folder structure

--- src/contexts/AuthContext.js
+++ src/contexts/AuthContext.js
@@ -1,6 +1,6 @@
-import React, { useState } from "react";
+import React, { createContext, useState } from "react";
 
-const AuthContext = React.createContext();
+const AuthContext = createContext();
 
 export default AuthContext;
 
