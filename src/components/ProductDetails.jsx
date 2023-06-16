50f0823df0d2df6749a2008b194f937f63228196
Abir Mondal <abir22197072@gmail.com>
2023-06-16 15:52:51 +0530

    Add to cart from product details page

--- src/components/ProductDetails.jsx
+++ src/components/ProductDetails.jsx
@@ -1,16 +1,37 @@
-import React, { useEffect, useState, useRef } from "react";
+import React, { useEffect, useState, useRef, useContext } from "react";
 import { useParams } from "react-router-dom";
+import { addToCart, removeFromCart, getCartItems, getCartItemsCount, getCartItemIDCount } from "../actions/cartActions";
+import CartContext from "../contexts/CartContext";
 
 export default function ProductDetails() {
   const [product, setProduct] = useState(null);
   const { id } = useParams();
   const rateCeilRef = useRef(0);
+  const { setCartCount, setCartItems } = useContext(CartContext);
+  const [quantity, setQuantity] = useState(getCartItemIDCount(Number(id)));
+  
+  function updateCartValues() {
+    setQuantity(getCartItemIDCount(Number(id)));
+    setCartItems(getCartItems());
+    setCartCount(getCartItemsCount());
+  }
+
+  function handleAddToCart() {
+    addToCart(product);
+    updateCartValues();
+  }
+
+  function handleRemoveFromCart() {
+    removeFromCart(product.id);
+    updateCartValues();
+  }
 
   useEffect(() => {
     const fetchProduct = async (id) => {
       try {
         const response = await fetch(`https://fakestoreapi.com/products/${id}`);
         const data = await response.json();
+        data.quantity = quantity;
         setProduct(data);
         rateCeilRef.current = Math.round(data.rating.rate);
       } catch (error) {
@@ -18,7 +39,7 @@ export default function ProductDetails() {
       }
     };
     fetchProduct(id);
-  }, [id]);
+  }, [id, quantity]);
 
   if (!product) {
     return (
@@ -47,9 +68,26 @@ export default function ProductDetails() {
           {product.rating.count}
         </div>
         <h3>${product.price}</h3>
-        <button className="btn btn-primary mt-3" type="button">
+        {/* <button className="btn btn-primary mt-3" type="button">
           Add to Cart
-        </button>
+        </button> */}
+        {quantity === 0 ? (
+          <div className="input-group mt-3">
+            <button className="btn btn-primary" type="button" onClick={handleAddToCart}>
+              Add to Cart
+            </button>
+          </div>
+        ) : (
+          <div className="input-group mt-3" style={{width: "100px"}}>
+            <button className="btn btn-secondary btn-sm" onClick={handleRemoveFromCart}>
+              -
+            </button>
+            <input type="text" className="form-control quantityCount" aria-label="Quantity of product" value={quantity} disabled />
+            <button className="btn btn-primary btn-sm" onClick={handleAddToCart}>
+              +
+            </button>
+          </div>
+        )}
       </div>
     </div>
   );
